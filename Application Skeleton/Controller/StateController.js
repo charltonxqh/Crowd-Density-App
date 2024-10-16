2029259595dc9a805b5dec01e3112c55d522055e
LeowYiShian <leowyishian@gmail.com>
2024-10-16 23:21:39 +0800

    update on controller

--- Application Skeleton/Controller/StateController.js
+++ Application Skeleton/Controller/StateController.js
@@ -2,35 +2,12 @@
  * The control class for managing application state.
  */
 const StateController = () => {
-    
-    /**
-     * Set the user in the application state.
-     * @param {Object} user The user object to be set.
-     */
-    const setUser = (user) => { }
 
     /**
      * Select an MRT station in the application state.
      * @param {String} station The station identifier to be selected.
      */
     const selectStation = (station) => { }
-
-    /**
-     * Get the current user authentication state from the application state.
-     * @returns {Object} The currently authenticated user.
-     */
-    const getUser = () => { }
-
-    /**
-     * Get the currently selected MRT station.
-     * @returns {String} The identifier of the selected MRT station.
-     */
-    const getSelectedStation = () => { }
-
-    /**
-     * Reset the application state to its initial values.
-     */
-    const resetState = () => { }
 }
 
 export default StateController;
