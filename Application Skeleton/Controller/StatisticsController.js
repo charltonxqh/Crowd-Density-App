aa657014413950a5c36c28b4ffd8cfee372978fc
charltonxqh <csiaw001@e.ntu.edu.sg>
2024-10-19 14:31:45 +0800

    done controller skeleton

--- Application Skeleton/Controller/StatisticsController.js
+++ Application Skeleton/Controller/StatisticsController.js
@@ -3,26 +3,18 @@ import React from 'react'
  * Control class responsible for managing and updating crowd density statistics.
  */
 const StatisticsController = () => {
-    
     /**
-     * A map that stores historical crowd density data.
-     * @type {Map[date, CrowdDensity]}
-     */
-    const historicalData = new Map(); // Initialize the map to store historical crowd density data.
-
-    /**
-     * Fetches crowd density data for a specific station.
-     * @param {Station} station - The station for which crowd density data is fetched.
-     * @returns {CrowdDensity[]} - A list of crowd density data.
-     */
-    const fetchCrowdDensityData = (station) => { };
-
-    /**
-     * Updates crowd density trends based on new data for a specific station.
+     * Updates crowd density trends based on latest data for a specific station.
      * @param {Station} station - The station for which the crowd density trends are updated.
      * @returns {CrowdDensity[]} - A list of updated crowd density data.
      */
     const updateCrowdDensityTrends = (station) => { };
+    
+    /**
+     * Updates leaderboard based on latest crowd density data.
+     * @returns {Leaderboard} - A list of updated crowd density data.
+     */
+    const updateLeaderboard = () => { };
 
 };
 
