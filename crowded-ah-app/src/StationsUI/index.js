33d4e39f5ac4c871db8e6cc877601c336625072b
Liaw Rui Xian <ruixianliaw@gmail.com>
2024-10-19 10:07:09 +0800

    skeleton updated

--- crowded-ah-app/src/StationsUI/index.js
+++
@@ -1,9 +0,0 @@
-const MrtLrtUI = () => {
-    return (
-        <div>
-            <h1>This is MRT/LRT page</h1>
-        </div>
-    )
-}
-
-export default MrtLrtUI
--- crowded-ah-app/src/components/Map.js
+++ crowded-ah-app/src/components/Map.js
@@ -1,12 +1,19 @@
-// src/App.js
+// src/Map.js
 import React from 'react';
 import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 const App = () => (
   <APIProvider apiKey={'AIzaSyBVamnyuIq-K7dy7S5w8RTxctHc5Oafb6w'} onLoad={() => console.log('Maps API has loaded.')}>
-    <div style={{ width: '100%', height: '400px'}}>
+    <div style={{ 
+    width: '30%', 
+    height: '300px', 
+    border: '2px solid white', 
+    borderRadius: '25px',
+    margin: '50px', 
+    display: 'flex'
+}}>
       <Map
-        defaultZoom={11}
+        defaultZoom={11.5}
         defaultCenter={{ lat: 1.3521, lng: 103.8198 }}
         onCameraChanged={(ev) =>
           console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
