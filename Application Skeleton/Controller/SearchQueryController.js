add6b859df5de8cfbe83715ed08cf519381375f1
Liaw Rui Xian <ruixianliaw@gmail.com>
2024-10-19 15:32:54 +0800

    a

--- Application Skeleton/Controller/SearchQueryController.js
+++ Application Skeleton/Controller/SearchQueryController.js
@@ -6,6 +6,13 @@ const SearchQueryController = () => {
      * @returns {List} - An array of search results.
      */
     const performSearch = (query) => {}
+    /**
+   * Adds a station to the user's list of favorite stations.
+   * 
+   * @param {Object} station - The station object to add.
+   * @returns {boolean} - Returns true if the station was added successfully.
+   */
+    const addFavoriteStation = (station) => {}
 }
 export default SearchQueryController;
   
