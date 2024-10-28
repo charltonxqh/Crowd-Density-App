61cab7235d789f9f73c1bb54112fd96290cd46bf
charltonxqh <csiaw001@e.ntu.edu.sg>
2024-10-29 00:23:20 +0800

    create stationsInfo.json

--- crowded-ah-app/src/HomeUI/home.js
+++ crowded-ah-app/src/HomeUI/home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TopBar from "../components/TopBar"
 import NearbyStationMap from '../components/NearbyStationMap';
-import NearbyStationList from '../components/NearbyStationList';
 
 const HomeUI = () => {
     return (
