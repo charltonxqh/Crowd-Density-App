3638f74ec6b4699690ac8c3f46ef2732cffd474c
yiken0724 <e.yiken@gmail.com>
2024-10-30 01:53:14 +0800

    user will not be redirected to home page after clicking icon on signup login page

--- crowded-ah-app/src/components/Logo.js
+++ crowded-ah-app/src/components/Logo.js
@@ -1,15 +1,26 @@
 import './Logo.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Logo() {
+    const location = useLocation();
+    const isAuthPage = location.pathname === '/';
+
     return (
         <div className='logo'>
-            <Link to="/home">
-                <img src="/images/Logo.png" alt="Logo"/>
-                <span className="logo-text">Crowded Ah?</span>
-            </Link>
+            {isAuthPage ? (
+                <>
+                    <img src="/images/Logo.png" alt="Logo"/>
+                    <span className="logo-text">Crowded Ah?</span>
+                </>
+            ) : (
+                <Link to="/home">
+                    <img src="/images/Logo.png" alt="Logo"/>
+                    <span className="logo-text">Crowded Ah?</span>
+                </Link>
+            )}
         </div>
     );
 }
 
 export default Logo;
+
