41c2e9b806feac2e1fe07708e954260be9e90205
charltonxqh <csiaw001@e.ntu.edu.sg>
2024-11-01 14:38:21 +0800

    arrange profileIcon position

--- crowded-ah-app/src/HomeUI/home.js
+++ crowded-ah-app/src/HomeUI/home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import TopBar from "../components/TopBar"
 import NearbyStationMap from '../components/NearbyStationMap';
 
 const HomeUI = () => {
     return (
         <div>
-            <TopBar />
             <NearbyStationMap />
         </div>
     )
